6adb2728d4bca0ac32f1d5bcef8fece6998b8030
christianrp <christianr@ymail.com>
2016-11-09 21:19:27 -0800

    linked javascript file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const colors = [
+  'rgb(255, 0, 0)',
+  'rgb(0, 255, 0)',
+  'rgb(0, 0, 255)',
+  'rgb(255, 255, 0)',
+  'rgb(255, 0, 255)',
+  'rgb(0, 255, 255)'
+]
+
+let squares = document.querySelectorAll('square');
+
+colors.forEach(color => {console.log(color)});
